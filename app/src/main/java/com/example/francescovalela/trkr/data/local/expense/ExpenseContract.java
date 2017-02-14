822e86c764bc305c8f8d72838eff5485840fe42b
Francesco Valela <valela.francesco@gmail.com>
2017-02-14 10:20:28 -0500

    init commit

--- app/src/main/java/com/example/francescovalela/trkr/data/local/expense/ExpenseContract.java
+++ app/src/main/java/com/example/francescovalela/trkr/data/local/expense/ExpenseContract.java
@@ -23,44 +23,4 @@ public class ExpenseContract  {
         public static final String COLUMN_NAME_METHODOFPAYMENTID = "method_of_payment_id";
         public static final String COLUMN_NAME_CATEGORYID = "category_id";
     }
-
-    private static final String SQL_CREATE_ENTRIES_EXPENSE =
-            "CREATE TABLE " + ExpenseEntry.TABLE_NAME + " (" +
-                    ExpenseEntry._ID + " INTEGER PRIMARY KEY," +
-                    ExpenseEntry.COLUMN_NAME_DATE + " INTEGER NOT NULL," +
-                    ExpenseEntry.COLUMN_NAME_NAME + " TEXT NOT NULL," +
-                    ExpenseEntry.COLUMN_NAME_COST + " REAL NOT NULL," +
-                    ExpenseEntry.COLUMN_NAME_METHODOFPAYMENTID + " INTEGER NOT NULL," +
-                    ExpenseEntry.COLUMN_NAME_CATEGORYID + " INTEGER NOT NULL" +
-                    ");";
-
-    private static final String SQL_CREATE_ENTRIES_CATEGORY =
-            "CREATE TABLE " + CategoryEntry.TABLE_NAME + " (" +
-                    CategoryEntry._ID + " INTEGER PRIMARY KEY," +
-                    CategoryEntry.COLUMN_NAME_ID + " INTEGER NOT NULL," +
-                    CategoryEntry.COLUMN_NAME_NAME + " TEXT NOT NULL" +
-                    ");";
-
-    private static final String SQL_CREATE_ENTRIES_METHODOFPAYMENT =
-            "CREATE TABLE " + MethodOfPaymentEntry.TABLE_NAME + " (" +
-                    MethodOfPaymentEntry._ID + " INTEGER PRIMARY KEY," +
-                    MethodOfPaymentEntry.COLUMN_NAME_NICKNAME + " TEXT NOT NULL," +
-                    MethodOfPaymentEntry.COLUMN_NAME_TYPEID + " INTEGER NOT NULL," +
-                    MethodOfPaymentEntry.COLUMN_NAME_DATE + " INTEGER NOT NULL" +
-                    ");";
-
-    private static final String SQL_CREATE_ENTRIES_METHODOFPAYMENTTYPE =
-            "CREATE TABLE " + MethodOfPaymentTypeEntry.TABLE_NAME + " (" +
-                    MethodOfPaymentTypeEntry._ID + " INTEGER PRIMARY KEY," +
-                    MethodOfPaymentTypeEntry.COLUMN_NAME_ID + " INTEGER NOT NULL," +
-                    MethodOfPaymentTypeEntry.COLUMN_NAME_NAME + " TEXT NOT NULL" +
-                    ");";
-
-
-    private static final String SQL_DELETE_ENTRIES =
-            "DROP TABLE IF EXISTS " + ExpenseEntry.TABLE_NAME + ", " +
-                    CategoryEntry.TABLE_NAME + ", " +
-                    MethodOfPaymentEntry.TABLE_NAME + ", " +
-                    MethodOfPaymentTypeEntry.TABLE_NAME;
-
 }
