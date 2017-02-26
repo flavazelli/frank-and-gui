dbeefe2622829dec4ff80eb3443946bf7ab03afe
Francis Lavazelli <francis@gradeslam.org>
2017-02-26 14:57:45 -0500

    coded method of payment type repository

--- app/src/main/java/com/example/francescovalela/trkr/data/local/MethodOfPaymentTypeRepository.java
+++ app/src/main/java/com/example/francescovalela/trkr/data/local/MethodOfPaymentTypeRepository.java
@@ -2,7 +2,15 @@ package com.example.francescovalela.trkr.data.local;
 
 import android.support.annotation.NonNull;
 
+import com.example.francescovalela.trkr.data.local.methodofpayment.MethodOfPaymentLocalDataSource;
+import com.example.francescovalela.trkr.data.local.methodofpaymenttype.MethodOfPaymentTypeLocalDataSource;
 import com.example.francescovalela.trkr.logExpense.models.MethodOfPaymentType;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static android.support.test.espresso.core.deps.guava.base.Preconditions.checkNotNull;
 
 /**
  * Created by Francis on 2017-02-23.
@@ -10,28 +18,81 @@ import com.example.francescovalela.trkr.logExpense.models.MethodOfPaymentType;
 
 public class MethodOfPaymentTypeRepository implements MethodOfPaymentTypeDataSource {
 
+    private static MethodOfPaymentTypeRepository INSTANCE = null;
+
+    private final MethodOfPaymentTypeDataSource mMethodOfPaymentTypeLocalDataSource;
+
+    Map<String, MethodOfPaymentType> typeMap;
+
+    private MethodOfPaymentTypeRepository(@NonNull MethodOfPaymentTypeDataSource methodOfPaymentTypeLocalDataSource) {
+        mMethodOfPaymentTypeLocalDataSource = checkNotNull(methodOfPaymentTypeLocalDataSource);
+    }
+
+
+    // Returns the single instance of this class, creating it if necessary.
+    public static MethodOfPaymentTypeRepository getInstance(@NonNull MethodOfPaymentTypeDataSource methodOfPaymentTypeDataSource) {
+        if (INSTANCE == null) {
+            INSTANCE = new MethodOfPaymentTypeRepository(methodOfPaymentTypeDataSource);
+        }
+        return INSTANCE;
+    }
+
     @Override
-    public void getMethodOfPaymentTypes(@NonNull LoadMethodOfPaymentTypesCallback callback) {
+    public void getMethodOfPaymentTypes(@NonNull final LoadMethodOfPaymentTypesCallback callback) {
+        mMethodOfPaymentTypeLocalDataSource.getMethodOfPaymentTypes(new LoadMethodOfPaymentTypesCallback() {
+            @Override
+            public void onMethodOfPaymentTypesLoaded(List<MethodOfPaymentType> expenses) {
+                callback.onMethodOfPaymentTypesLoaded(new ArrayList<>(typeMap.values()));
+            }
+
+            @Override
+            public void onDataNotAvailable() {
+                System.out.print("No data source connected");
+            }
+
+        });
 
     }
 
     @Override
-    public void getMethodOfPaymentType(@NonNull String typeId, @NonNull GetMethodOfPaymentTypeCallback callback) {
+    public void getMethodOfPaymentType(@NonNull final String typeId, @NonNull final GetMethodOfPaymentTypeCallback callback) {
+        checkNotNull(typeId);
+        checkNotNull(callback);
+        mMethodOfPaymentTypeLocalDataSource.getMethodOfPaymentType(typeId, new GetMethodOfPaymentTypeCallback() {
+            @Override
+            public void onMethodOfPaymentTypeLoaded(MethodOfPaymentType expense) {
+                typeMap.put(String.valueOf(expense.getId()), expense);
+                callback.onMethodOfPaymentTypeLoaded(expense);
+            }
+
+            @Override
+            public void onDataNotAvailable() {
+                System.out.print("No data source connected");
+            }
+        });
 
     }
 
     @Override
     public void saveMethodOfPaymentType(@NonNull MethodOfPaymentType type) {
-
+        checkNotNull(type);
+        mMethodOfPaymentTypeLocalDataSource.saveMethodOfPaymentType(type);
     }
 
     @Override
     public void deleteAllMethodOfPaymentTypes() {
-
+        mMethodOfPaymentTypeLocalDataSource.deleteAllMethodOfPaymentTypes();
     }
 
     @Override
     public void deleteMethodOfPaymentType(@NonNull String typeId) {
+        mMethodOfPaymentTypeLocalDataSource.deleteMethodOfPaymentType(typeId);
+    }
 
+    private void refreshLocalDataSource(List<MethodOfPaymentType> types) {
+        mMethodOfPaymentTypeLocalDataSource.deleteAllMethodOfPaymentTypes();
+        for (MethodOfPaymentType type : types) {
+            mMethodOfPaymentTypeLocalDataSource.saveMethodOfPaymentType(type);
+        }
     }
 }
