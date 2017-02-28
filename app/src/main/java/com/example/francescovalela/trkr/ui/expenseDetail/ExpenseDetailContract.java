a119ce35aaddb907a71c54ed02214469a0c6de0f
Francesco Valela <valela.francesco@gmail.com>
2017-02-28 11:41:27 -0500

    filling in AddExpense Presenter class

--- app/src/main/java/com/example/francescovalela/trkr/ui/expenseDetail/ExpenseDetailContract.java
+++ app/src/main/java/com/example/francescovalela/trkr/ui/expenseDetail/ExpenseDetailContract.java
@@ -1,8 +1,27 @@
 package com.example.francescovalela.trkr.ui.expenseDetail;
 
+import com.example.francescovalela.trkr.BaseView;
+import com.example.francescovalela.trkr.BasePresenter;
+
+import static android.R.id.edit;
+
 /**
  * Created by francescovalela on 2017-02-28.
+ * Contract that sets up interaction between view and presenter.
+ * For viewing a single expense log in detail
  */
 
 public interface ExpenseDetailContract {
+
+    interface View extends BaseView<Presenter> {
+
+        void showDetails();
+
+    }
+
+    interface Presenter extends BasePresenter {
+
+        void editExpense();
+
+    }
 }
