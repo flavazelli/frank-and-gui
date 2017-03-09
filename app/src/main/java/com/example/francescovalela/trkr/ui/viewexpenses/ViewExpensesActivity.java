4ec32dbae356057e53fe523114d34b57f0b2fd30
Francis Lavazelli <francis@gradeslam.org>
2017-03-09 09:52:44 -0500

    hey

--- app/src/main/java/com/example/francescovalela/trkr/ui/viewexpenses/ViewExpensesActivity.java
+++ app/src/main/java/com/example/francescovalela/trkr/ui/viewexpenses/ViewExpensesActivity.java
@@ -1,7 +1,13 @@
 package com.example.francescovalela.trkr.ui.viewexpenses;
 
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.francescovalela.trkr.R;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by flavazelli on 2017-02-28.
@@ -9,9 +15,23 @@ import android.os.Bundle;
 
 public class ViewExpensesActivity extends Activity {
 
+    private ViewExpensesPresenter mViewExpensesPresenter;
+    FragmentManager fragmentManager = getFragmentManager();
+    ViewExpensesFragment viewExpensesFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        ViewExpensesFragment viewExpensesFragment =  new ViewExpensesFragment();
+
+        fragmentManager.beginTransaction()
+                .add(R.id.fragment_container, viewExpensesFragment)
+                .commit();
+
+        // Create the presenter
+        mViewExpensesPresenter = new ViewExpensesPresenter(viewExpensesFragment, getApplicationContext());
     }
 
     @Override
--- app/src/main/java/com/example/francescovalela/trkr/ui/viewexpenses/ViewExpensesFragment.java
+++ app/src/main/java/com/example/francescovalela/trkr/ui/viewexpenses/ViewExpensesFragment.java
@@ -4,14 +4,20 @@ import android.app.ListFragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
+import com.example.francescovalela.trkr.R;
 import com.example.francescovalela.trkr.logExpense.models.Expense;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static android.content.ContentValues.TAG;
 import static android.support.test.espresso.core.deps.guava.base.Preconditions.checkNotNull;
 
 /**
@@ -22,10 +28,23 @@ public class ViewExpensesFragment extends ListFragment
     implements ViewExpensesContract.View {
 
     ViewExpensesContract.Presenter mPresenter;
+    ExpensesAdapter expenseAdapter;
+
+    public ViewExpensesFragment() {
+        // Requires empty public constructor
+    }
+
+    public static ViewExpensesFragment newInstance() {
+        return new ViewExpensesFragment();
+    }
+
 
     @Override
     public void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+        expenseAdapter = new ExpensesAdapter(new ArrayList<Expense>(0));
+        setListAdapter(expenseAdapter);
     };
 
     @Override
@@ -40,8 +59,9 @@ public class ViewExpensesFragment extends ListFragment
 
     @Override
     public void onResume() {
-        mPresenter.start();
         super.onResume();
+
+        mPresenter.start();
     }
 
     @Override
@@ -52,7 +72,7 @@ public class ViewExpensesFragment extends ListFragment
 
     @Override
     public void showExpenses(List<Expense> expenses) {
-
+        expenseAdapter.replaceData(expenses);
     }
 
     @Override
@@ -73,6 +93,63 @@ public class ViewExpensesFragment extends ListFragment
 
     @Override
     public void showExpenseError() {
+
+    }
+
+    private class ExpensesAdapter extends ArrayAdapter<Expense> {
+
+        private List<Expense> mExpenses;
+
+        public ExpensesAdapter(List<Expense> expenses)
+        {
+            super(getActivity(),0, expenses);
+            setList(expenses);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Log.d(TAG, "GET VIEW:" + convertView.toString());
+
+            //if we weren't given a  view, inflate one
+            if (convertView == null) {
+                convertView = getActivity().getLayoutInflater()
+                        .inflate(R.layout.fragment_expense_list, null);
+            }
+
+            //Configure the view for this expense
+            Expense e = getItem(position);
+            TextView dateTextView = (TextView) convertView.findViewById(R.id.date);
+            TextView costTextView = (TextView) convertView.findViewById(R.id.cost);
+            TextView categoryTextView = (TextView) convertView.findViewById(R.id.category);
+
+            dateTextView.setText(e.getDate().toString());
+            costTextView.setText(String.valueOf(e.getCost()));
+            //TODO: create category methods in expense
+            categoryTextView.setText(e.getName());
+
+            return convertView;
+        }
+
+        public void replaceData(List<Expense> expenses) {
+            setList(expenses);
+            notifyDataSetChanged();
+        }
+
+        private void setList(List<Expense> expenses) {
+            mExpenses = checkNotNull(expenses);
+        }
+
+        @Override
+        public Expense getItem(int i) {
+            return mExpenses.get(i);
+        }
+
+        @Override
+        public int getCount() {
+            return mExpenses.size();
+        }
+
+
 
     }
 }
